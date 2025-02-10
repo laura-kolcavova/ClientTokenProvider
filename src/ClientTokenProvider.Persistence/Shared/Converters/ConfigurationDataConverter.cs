8a8af17545447bd2d35b5b1f64a0ba00f28b04c4
Laura Kolčavová <laura.kolcavova@notino.com>
2025-02-10 22:08:11 +0100

    fix: project changes

---
+++ src/ClientTokenProvider.Persistence/Shared/Converters/ConfigurationDataConverter.cs
@@ -0,0 +1,36 @@
+﻿//using ClientTokenProvider.Business.Shared.Models.Abstractions;
+//using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+//using System.Text.Json;
+
+//namespace ClientTokenProvider.Persistence.Shared.Converters;
+
+
+//internal sealed class ConfigurationDataConverter :
+//    ValueConverter<IConfigurationData, string>
+//{
+//    public ConfigurationDataConverter(
+//        JsonSerializerOptions? jsonSerializerOptions = null)
+//        : base(
+//            value => Serialize(value, jsonSerializerOptions),
+//            value => Deserialize(value, jsonSerializerOptions))
+//    {
+//    }
+
+//    private static string Serialize(
+//     IConfigurationData value,
+//     JsonSerializerOptions? jsonSerializerOptions = null)
+//    {
+//        return JsonSerializer.Serialize(
+//            value,
+//            jsonSerializerOptions);
+//    }
+
+//    private static IConfigurationData Deserialize(
+//        string value,
+//        JsonSerializerOptions? jsonSerializerOptions = null)
+//    {
+//        return JsonSerializer.Deserialize<IConfigurationData>(
+//            value,
+//            jsonSerializerOptions)!;
+//    }
+//}
