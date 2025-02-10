6dcc10f0dd1ed6ff4f131af286ccc46a63c8810f
Laura Kolčavová <laura.kolcavova@notino.com>
2025-02-10 20:10:15 +0100

    fix: minor changes

--- src/ClientTokenProvider/Shared/Controls/JwtVisualizer.xaml.cs
+++ src/ClientTokenProvider/Shared/Controls/JwtVisualizer.xaml.cs
@@ -1,5 +1,6 @@
 using ClientTokenProvider.Business.Shared.Models;
 using ClientTokenProvider.Shared.Models;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ClientTokenProvider.Shared.Controls;
 
@@ -20,6 +21,11 @@ public partial class JwtVisualizer : ContentView
        typeof(JwtVisualizationMode),
        typeof(JwtVisualizer));
 
+    public static readonly BindableProperty HandleVisualizationModeChangedCommandProperty = BindableProperty.Create(
+       nameof(HandleVisualizationModeChangedCommand),
+       typeof(IRelayCommand<JwtVisualizationMode>),
+       typeof(JwtVisualizer));
+
     public string Token
     {
         get => (string)GetValue(TokenProperty);
@@ -38,13 +44,34 @@ public partial class JwtVisualizer : ContentView
         set => SetValue(VisualizationModeProperty, value);
     }
 
-    public List<JwtVisualizationMode> VisualizationModes { get; } = Enum
+    public IReadOnlyCollection<JwtVisualizationMode> VisualizationModes { get; } = Enum
         .GetValues(typeof(JwtVisualizationMode))
         .Cast<JwtVisualizationMode>()
         .ToList();
 
+    // Binding to ConfigurationDetail.AccessTokenVisualizationMode actually does not work in two way mode
+    public IRelayCommand<JwtVisualizationMode> HandleVisualizationModeChangedCommand
+    {
+        get => (IRelayCommand<JwtVisualizationMode>)GetValue(HandleVisualizationModeChangedCommandProperty);
+        set => SetValue(HandleVisualizationModeChangedCommandProperty, value);
+    }
+
     public JwtVisualizer()
     {
         InitializeComponent();
+    }
+
+    private void VisualizationModePicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        var selectedIndex = picker.SelectedIndex;
+
+        if (selectedIndex != -1)
+        {
+            var newVisualizationMode = VisualizationModes.ElementAt(selectedIndex);
+
+            // Setting VisualizationMode will broke UI when switching Configurations so we have to use command.
+            HandleVisualizationModeChangedCommand?.Execute(newVisualizationMode);
+        }
     }
 }
